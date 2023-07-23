2d3f5a270631912918daaebb910ffadee2b36998
Mertier <merocks86@gmail.com>
2023-07-24 00:54:54 +0300

    Fixed indentation consistency across files

--- cogs/exchange.py
+++ cogs/exchange.py
@@ -5,39 +5,39 @@ import aiohttp
 
 
 class Exchange(commands.Cog):
-    def __init__(self, bot):
-        self.bot = bot
+  def __init__(self, bot):
+    self.bot = bot
 
-    @commands.command()
-    async def doviz(self, ctx: commands.Context):
+  @commands.command()
+  async def doviz(self, ctx: commands.Context):
 
-        url = "https://dovizborsa.com/"
+    url = "https://dovizborsa.com/"
 
-        async with aiohttp.ClientSession() as session:
-            async with session.get(url) as response:
-                if response.status == 200:
-                    soup = BeautifulSoup(await response.text(),"html.parser")
+    async with aiohttp.ClientSession() as session:
+      async with session.get(url) as response:
+        if response.status == 200:
+          soup = BeautifulSoup(await response.text(),"html.parser")
 
-                else:
-                    ctx.send("Döviz verisine erişilemedi.")
-                    return 0
+        else:
+          ctx.send("Döviz verisine erişilemedi.")
+          return 0
 
-        keys = ["435","200","751"] # ID's of dollar, euro, and gold
+    keys = ["435","200","751"] # ID's of dollar, euro, and gold
 
-        values = []
+    values = []
 
-        for key in keys:
-            alimsatim = soup.find("div",id=key).find("div",class_="-x1").find_all("span")
-            for value in alimsatim:
-                values.append(value.get_text(strip=True)[:-2])
+    for key in keys:
+      alimsatim = soup.find("div",id=key).find("div",class_="-x1").find_all("span")
+      for value in alimsatim:
+        values.append(value.get_text(strip=True)[:-2])
 
-        embed = discord.Embed(title="Döviz", color=discord.Colour.blurple())
+    embed = discord.Embed(title="Döviz", color=discord.Colour.blurple())
 
-        embed.add_field(name="Dolar",value=f"Alım: {values[1]}\nSatım: {values[0]}", inline=True)
-        embed.add_field(name="Euro",value=f"Alım: {values[3]}\nSatım: {values[2]}", inline=True)
-        embed.add_field(name="Altın (gr)",value=f"Alım: {values[5]}\nSatım: {values[4]}", inline=True)
-        await ctx.send(embed=embed)
+    embed.add_field(name="Dolar",value=f"Alım: {values[1]}\nSatım: {values[0]}", inline=True)
+    embed.add_field(name="Euro",value=f"Alım: {values[3]}\nSatım: {values[2]}", inline=True)
+    embed.add_field(name="Altın (gr)",value=f"Alım: {values[5]}\nSatım: {values[4]}", inline=True)
+    await ctx.send(embed=embed)
 
 
 async def setup(bot):
-    await bot.add_cog(Exchange(bot))
+  await bot.add_cog(Exchange(bot))
