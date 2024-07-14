f6c6d59a6d504b13d8471e1cff91616c02f3ec0e
oralet86 <merocks86@gmail.com>
2024-07-14 23:05:40 +0300

    Updated environmental_variables.py to enforce putting values in the .env file

--- environmental_variables.py
+++ environmental_variables.py
@@ -2,9 +2,20 @@ from dotenv import load_dotenv
 import os
 
 load_dotenv(".env")
-TOKEN = os.getenv("TOKEN")
-PREFIX = os.getenv("PREFIX")
-TEST_CHANNEL = os.getenv("TEST_CHANNEL")
-SEARCH_COOLDOWN = os.getenv("SEARCH_COOLDOWN")
-DB_DIRECTORY = os.getenv("DB_DIRECTORY")
-DH_DOMAIN = os.getenv("DH_DOMAIN")
+TOKEN = str(os.getenv("TOKEN", ""))
+assert TOKEN != "", "TOKEN is not set in .env"
+
+PREFIX = str(os.getenv("PREFIX", ""))
+assert PREFIX != "", "PREFIX is not set in .env"
+
+DB_DIRECTORY = str(os.getenv("DB_DIRECTORY", ""))
+assert DB_DIRECTORY != "", "DB_DIRECTORY is not set in .env"
+
+DH_DOMAIN = str(os.getenv("DH_DOMAIN", ""))
+assert DH_DOMAIN != "", "DH_DOMAIN is not set in .env"
+
+TEST_CHANNEL = int(os.getenv("TEST_CHANNEL", -1))
+assert TEST_CHANNEL != -1, "TEST_CHANNEL is not set in .env"
+
+SEARCH_COOLDOWN = int(os.getenv("SEARCH_COOLDOWN", -1))
+assert SEARCH_COOLDOWN != -1, "SEARCH_COOLDOWN is not set in .env"
